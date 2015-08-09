065ab264eeb6c5cb7829b2201a5c476f3af8af2a
renato1958 <felisi.renato@gmail.com>
2015-08-09 22:29:43 +0200

    Aggiunti tasks serve e default a gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,12 @@
 var gulp = require('gulp');
 var browsersync = require('browser-sync');
 
-gulp.task('default', []);
+gulp.task('serve', function() {
+	browsersync.init({
+		server: './'
+	});
+
+	gulp.watch('*.html').on('change', browsersync.reload);
+})
+
+gulp.task('default', ['serve']);
